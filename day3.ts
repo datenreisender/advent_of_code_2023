a7ad22aa28eb5f2f0b694f208782d246cb5fec80
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-12-06 14:59:09 +0100

    Day 3, part 2

--- day3.ts
+++ day3.ts
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 // prettier-ignore
-import { values, toPairs, splitEvery, range, reduce, maxBy, minBy, prop, equals, sum, isEmpty, complement, propEq, either, times, propOr, __, pathOr, insert, repeat, zip, flatten, remove, over, add, lensIndex, scan, clone, contains, dropLast, pipe, identity, evolve, subtract, concat, flip, replace, split, join, props, sortBy, forEach, last, map, path, pathEq, reject, compose, uniq, chain, sortWith, ascend, reverse, identical, filter, gt, curry, pluck, without, update, multiply, match, gte, keys, xprod, T, tap } from 'ramda' // eslint-disable-line no-unused-vars
+import { values, toPairs, splitEvery, range, reduce, maxBy, minBy, prop, equals, sum, isEmpty, complement, propEq, either, times, propOr, __, pathOr, insert, repeat, zip, flatten, remove, over, add, lensIndex, scan, clone, contains, dropLast, pipe, identity, evolve, subtract, concat, flip, replace, split, join, props, sortBy, forEach, last, map, path, pathEq, reject, compose, uniq, chain, sortWith, ascend, reverse, identical, filter, gt, curry, pluck, without, update, multiply, match, gte, keys, xprod, T, tap, find, assoc } from 'ramda' // eslint-disable-line no-unused-vars
 // prettier-ignore
 import { expect, describe, test, xtest, TODO, inputContent, inputContentLines, inputContentChars, lines, chars } from './setup' // eslint-disable-line no-unused-vars
 
@@ -93,8 +93,65 @@ test('acceptance of part 1', () => {
   expect(part1(inputContentLines(testInput))).toEqual(4361)
 })
 
+type NumberInField = {
+  value: number
+  line: number
+  start: number
+  end: number
+}
+
+const findNumbersInField = (input: string[]): NumberInField[] =>
+  input.flatMap((line, lineIndex) =>
+    findAllNumbers(line).map(assoc('line', lineIndex))
+  )
+
+const findStarsInField = (input: string[]): { line: number; pos: number }[] =>
+  input.flatMap((line, lineIndex) =>
+    [...line.matchAll(/\*/g)].map((match: RegExpMatchArray) => ({
+      line: lineIndex,
+      pos: match.index
+    }))
+  )
+
+const isNextTo = (line: number, pos: number) => (number: NumberInField) => {
+  const isOnRightLine = line >= number.line - 1 && line <= number.line + 1
+  const isRightBeforeStar = number.end + 1 === pos
+  const isRightAfterStar = number.start - 1 === pos
+  const isWithinStarRange = number.start <= pos && number.end >= pos
+
+  return (
+    isOnRightLine &&
+    (isRightBeforeStar || isRightAfterStar || isWithinStarRange)
+  )
+}
+
+const findAdjacentNumbers = (
+  numbers: NumberInField[],
+  line: number,
+  pos: number
+) => numbers.filter(isNextTo(line, pos)).map(prop('value'))
+
+const gearRatios = (
+  numbers: { value: number; line: number; start: number; end: number }[],
+  stars: { line: number; pos: number }[]
+) =>
+  stars.map(({ line, pos }) => {
+    const adjacentNumbers = findAdjacentNumbers(numbers, line, pos)
+
+    return adjacentNumbers.length === 2
+      ? adjacentNumbers[0] * adjacentNumbers[1]
+      : 0
+  })
+
+const part2 = (input: string[]) =>
+  sum(gearRatios(findNumbersInField(input), findStarsInField(input)))
+
+test('acceptance of part 2', () => {
+  expect(part2(inputContentLines(testInput))).toEqual(467835)
+})
+
 if (process.env.NODE_ENV !== 'test') {
   const input = inputContentLines()
   console.log('Part 1: ' + part1(input))
-  // console.log('Part 2: ' + part2(input))
+  console.log('Part 2: ' + part2(input))
 }
