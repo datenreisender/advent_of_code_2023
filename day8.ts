bf49af9afe53e2f7dfe704b8b232ef8ae29b9625
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-12-13 00:31:53 +0100

    Day 8, part 2

--- day8.ts
+++ day8.ts
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 // prettier-ignore
-import { values, toPairs, splitEvery, range, reduce, maxBy, minBy, prop, equals, sum, isEmpty, complement, propEq, either, times, propOr, __, pathOr, insert, repeat, zip, flatten, remove, over, add, lensIndex, scan, clone, contains, dropLast, pipe, identity, evolve, subtract, concat, flip, replace, split, join, props, sortBy, forEach, last, map, path, pathEq, reject, compose, uniq, chain, sortWith, ascend, reverse, identical, filter, gt, curry, pluck, without, update, multiply, match, gte, keys, xprod, T, tap } from 'ramda' // eslint-disable-line no-unused-vars
+import { values, toPairs, splitEvery, range, reduce, maxBy, minBy, prop, equals, sum, isEmpty, complement, propEq, either, times, propOr, __, pathOr, insert, repeat, ip, flatten, remove, over, add, lensIndex, scan, clone, contains, dropLast, pipe, identity, evolve, subtract, concat, flip, replace, split, join, props, sortBy, forEach, last, map, path, pathEq, reject, compose, uniq, chain, sortWith, ascend, reverse, identical, filter, gt, curry, pluck, without, update, multiply, match, gte, keys, xprod, T, tap, startsWith, endsWith, count, product } from 'ramda' // eslint-disable-line no-unused-vars
 // prettier-ignore
 import { expect, describe, test, xtest, TODO, inputContent, inputContentLines, inputContentChars, lines, chars } from './setup' // eslint-disable-line no-unused-vars
 
@@ -24,6 +24,44 @@ const part1 = (input?: string) => {
   } while (here != 'ZZZ')
 
   return steps
+}
+
+const locationAfterRound = (
+  start: string,
+  locations: Record<string, { L: string; R: string }>,
+  turns: string
+) =>
+  turns.split('').reduce<string>((here, turn) => locations[here][turn], start)
+
+const part2 = (input?: string) => {
+  const [turns, ...locationSpecs] = inputContentLines(input)
+
+  const locationMap = Object.fromEntries(locationSpecs.map(parseLocation))
+  const allLocations = Object.keys(locationMap)
+  const startLocations = allLocations.filter(endsWith('A'))
+
+  const locationAfterSingleRoundMap = Object.fromEntries(
+    allLocations.map((l: string) => [
+      l,
+      locationAfterRound(l, locationMap, turns)
+    ])
+  )
+
+  let rounds = 0
+  let here = startLocations
+  const roundsForLoop = {}
+
+  do {
+    here = here.map(h => locationAfterSingleRoundMap[h])
+    rounds++
+    here.forEach((h, index) => {
+      if (h.endsWith('Z')) {
+        roundsForLoop[startLocations[index]] = rounds
+      }
+    })
+  } while (startLocations.length !== Object.keys(roundsForLoop).length)
+
+  return product(Object.values(roundsForLoop)) * turns.length
 }
 
 const testInput1 = `
@@ -51,7 +89,24 @@ test('acceptance of part 1', () => {
   expect(part1(testInput2)).toEqual(6)
 })
 
+const testInput3 = `
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11)
+11 = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22, 22)
+22 = (22B, 22B)
+XXX = (XXX, XXX)
+`
+
+xtest('acceptance of part 2', () => {
+  expect(part2(testInput3)).toEqual(6)
+})
+
 if (process.env.NODE_ENV !== 'test') {
   console.log('Part 1: ' + part1())
-  // console.log('Part 2: ' + part2(input))
+  console.log('Part 2: ' + part2())
 }
